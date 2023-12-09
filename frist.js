428ee06b40690d257903932cb6ea9fad7a785592
IlOvecompUter1$t <shahva2022@gmail.com>
2023-12-09 09:59:08 +0500

    js

---
+++ frist.js
@@ -0,0 +1,64 @@
+// let age = prompt("apki age kia hy?")
+
+// if (age >= 60) {
+//     alert("you are senior citizen")
+// }
+// else if(age >= 40){
+//     alert("you are adult")
+// }
+// else if(age >= 25){
+//     alert("you are young man")
+// }
+// else if(age >= 13){
+//     alert("you are a Teenages")
+// }
+// else if(age >= 1){
+//     alert("you are a kid")
+// }
+// else{
+//     alert("your are a toodler")
+// }
+// let persentage = prompt("Enter your persentage.")
+
+// if (persentage >= 90) {
+//     alert("your grade is A+")
+// }
+// else if(persentage >= 80){
+//     alert("your grade is A")
+// }
+// else if(persentage >= 70){
+//     alert("your grade is B")
+// }
+// else if(persentage >= 60){
+//     alert("your grade is C")
+// }
+// else if(persentage >= 50){
+//     alert("your grade is D")
+// }
+// else{
+//     alert("you are failed")
+// }
+
+let price = prompt("Enter your purchase amount.")
+
+if (price >= 50000) {
+    alert("you got 20% discount")
+}
+else if(price >= 40000){
+    alert("you got 15% discount")
+}
+else if(price >= 30000){
+    alert("you got 12% discount")
+}
+else if(price >= 20000){
+    alert("you got 10% discount")
+}
+else if(price >= 15000){
+    alert("you got 5% discount")
+}
+else if(price >= 10000){
+    alert("you got 2% discount")
+}
+else{
+    alert("No discount")
+}
